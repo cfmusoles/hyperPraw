c0e41825f4a7215f0e46b86ba9cd75ec2790e377
bundenth <c.f.musoles@sheffield.ac.uk>
2018-10-29 19:49:59 +0000

    Added pseudocode skeleton

--- hyperPraw.py
+++ hyperPraw.py
@@ -5,3 +5,11 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 num_processes = comm.Get_size()
 process_id = comm.Get_rank()
+
+# Function to get the cost of a partition
+
+# Function for partitioning stats
+
+# SequentialStreamingPartitioning
+
+# ParallelStreamingPartitioning
--- hyperPraw_test.py
+++ hyperPraw_test.py
@@ -5,3 +5,15 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 num_processes = comm.Get_size()
 process_id = comm.Get_rank()
+
+## INPUT: hypergraph_input_file + max_iterations + imbalance_tolerance
+
+# Read hypergraph and get hyperedges
+
+# Broadcast random seed
+
+# create (or load) communication cost matrix
+
+# initialise vertex weights (can be 1)
+
+# Obtain partitioning calling hyperPraw.ParallelStreamingPartitioning()
