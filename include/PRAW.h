727aa63f902e251ac5fe25e09d3056d052837e04
bundenth <c.f.musoles@sheffield.ac.uk>
2019-03-13 13:41:33 +0000

    roll back cost function

--- include/PRAW.h
+++ include/PRAW.h
@@ -753,7 +753,7 @@ namespace PRAW {
                         }
                     }
                     
-                    double current_value =  (float)current_neighbours_in_partition[pp]/(float)total_neighbours - (double)total_comm_cost/(double)num_processes * comm_cost_per_partition[pp] - a * pow(part_load[pp],g-1);
+                    double current_value =  (float)current_neighbours_in_partition[pp] - (double)total_comm_cost * comm_cost_per_partition[pp] - a * pow(part_load[pp],g-1);
                     //double current_value =  (float)current_neighbours_in_partition[pp]/(float)total_neighbours - (double)total_comm_cost/(double)num_processes * comm_cost_per_partition[pp] - a * (part_load[pp]/expected_workload);
                     //double current_value = current_neighbours_in_partition[pp] -(double)total_comm_cost/(double)num_processes * comm_cost_per_partition[pp] - a * g/2 * pow(part_load[pp],g-1);
                     
